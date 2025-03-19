9ab0faf37619ce951ae756628cc9f00741c7bfe2
Hannah Freudenberger <hlfreudenberger@gmail.com>
2025-03-19 15:50:34 -0600

    fully init react project with jsx using babel and webpack

--- react-from-scratch/index.js
+++ react-from-scratch/index.js
@@ -1,1 +1,11 @@
-console.log('this is the react rendering');
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+
+const App = () => {
+  return <h1>This is a react script!</h1>
+}
+
+const container = document.getElementById('root');
+const root = createRoot(container);
+
+root.render(<App />)
